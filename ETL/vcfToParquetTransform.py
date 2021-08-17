07f328c94b9df0f0b781af5a231ac3a01fdffb0c
Rahul Chaturvedi <rahulch@amazon.com>
2021-08-17 13:17:45 -0700

    retrieving sample_id from vcf

--- ETL/vcfToParquetTransform.py
+++ ETL/vcfToParquetTransform.py
@@ -20,14 +20,16 @@ for i in sample_id:
 	#	continue
 	#else:
 		#sample_id = os.path.basename(filename).replace(".hard-filtered.vcf.bgz","")
-	sample_id_=str(i.split('/')[-1].split('.')[0])
+	#sample_id_=str(i.split('/')[-1].split('.')[0])
 	vds=hl.import_vcf(filename,reference_genome='GRCh38')
-	variant_table = vds.make_table()
-	v_spark=variant_table.to_spark()
-	v_spark_renamed=v_spark.toDF("locus.contig","locus.position","alleles","rsid","qual","filters",'info.AC','info.AF','info.AN','info.DB','info.DP','info.END','info.FS','info.FractionInformativeReads','info.LOD','info.MQ','info.MQRankSum','info.QD','info.R2_5P_bias','info.ReadPosRankSum','info.SOR',"AD","AF","DP","FIR2","F2R1","GP","GQ","GT.alleles","GT.phased","MB","PL","PRI","PS","SB","SQ")
-	v_spark_renamed=v_spark_renamed.withColumn("sample_id",lit(sample_id_))
-	v_spark_renamed=v_spark_renamed.withColumn('ref',v_spark_renamed.alleles[0]).withColumn('alt',v_spark_renamed.alleles[1])
-	v_spark_renamed = v_spark_renamed.withColumnRenamed("locus.contig","chrom").withColumnRenamed("locus.position","pos")
-	v_spark_renamed.createOrReplaceTempView("variant_v")
+	sample_id=vds.s.take(1)[0]
+	v_spark = vds.make_table().to_spark()
+	#v_spark_renamed=v_spark.toDF("locus.contig","locus.position","alleles","rsid","qual","filters",'info.AC','info.AF','info.AN','info.DB','info.DP','info.END','info.FS','info.FractionInformativeReads','info.LOD','info.MQ','info.MQRankSum','info.QD','info.R2_5P_bias','info.ReadPosRankSum','info.SOR',"AD","AF","DP","FIR2","F2R1","GP","GQ","GT.alleles","GT.phased","MB","PL","PRI","PS","SB","SQ")
+	for name in v_spark.schema.names:
+		if str(sample_id) in name:
+			v_spark = v_spark.withColumnRenamed(name,name.split('.',1)[1])
+	v_spark=v_spark.withColumn("sample_id",lit(sample_id)).withColumn('ref',v_spark.alleles[0]).withColumn('alt',v_spark.alleles[1])
+	v_spark = v_spark.withColumnRenamed("locus.contig","chrom").withColumnRenamed("locus.position","pos")
+	v_spark.createOrReplaceTempView("variant_v")
 	df=spark.sql("SELECT concat(chrom, ':', ref, ':', alt, ':', cast(pos as string)) as variant_id,* FROM variant_v where length(chrom)<6")
-	df.write.parquet('s3://ab3/dev_output_parquet/'+sample_id_+'/')
+	df.write.parquet('s3://ab3/dev_output_parquet_test/'+sample_id+'/')
